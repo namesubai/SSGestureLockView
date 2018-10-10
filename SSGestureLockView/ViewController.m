af3b1baccaf235623a85f7ee0e6d7245eec24798
yangshuquan <yangshuquan@zfhy.com>
2018-10-10 19:15:07 +0800

    上传

--- SSGestureLockView/ViewController.m
+++ SSGestureLockView/ViewController.m
@@ -7,16 +7,36 @@
 //
 
 #import "ViewController.h"
+#import "SSGestureLockView.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<SSGestureLockViewDelegate>
+{
+    NSString *password;
+    SSGestureLockView *gestureLockView;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    password = @"14789";
+    gestureLockView = [[SSGestureLockView alloc]initWithFrame:CGRectZero];
+    gestureLockView.delegate = self;
+    gestureLockView.frame = CGRectMake((CGRectGetWidth(self.view.frame)-240)/2, (CGRectGetHeight(self.view.frame)-240)/2, 240, 240);
+    [self.view addSubview:gestureLockView];
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didSelectedGestureLockView:(SSGestureLockView *)gestureLockView keyNumStr:(NSString *)keyNumStr{
+    NSLog(@"%@",keyNumStr);
+    if (![keyNumStr isEqualToString:password]) {
+        gestureLockView.showErrorStatus = YES;
+    }else{
+        NSLog(@"密码正确");
+    }
 }
 
 
